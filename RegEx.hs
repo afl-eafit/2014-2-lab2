7cfdda25b3e32f8164ae702acbefd94073621f02
dmonto3 <dmon501@gmail.com>
2014-09-07 17:10:22 -0500

    Update RegEx.hs

--- RegEx.hs
+++ RegEx.hs
@@ -1,7 +1,7 @@
-module RegEx where
-
 -- Taken from:
 -- http://www1.eafit.edu.co/asicard/courses/automata-CB0081/src/RegExp.html
+
+module RegEx where
 
 -- A polymorphic data regular expression data type
 data RegExp a = Empty
