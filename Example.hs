1030a86c68e1c3201f375adffa40b3b606f19ca4
dmonto3 <dmon501@gmail.com>
2014-09-07 17:09:22 -0500

    Example

---
+++ Example.hs
@@ -0,0 +1,17 @@
+module Example where
+
+import RegEx
+
+type RE = RegExp Char
+
+_0, _1 :: RE
+_0 = Symbol '0'
+_1 = Symbol '1'
+
+universe1, universe2 :: RE
+universe1 = Star (Plus _0 _1)
+universe2 = Star (Plus _1 _0)
+
+r, s :: RE
+r = Plus universe1 universe2
+s = Plus Empty (Epsilon `Dot` universe1)
