ad4e1c8b585f7d34f522937929d80a6564ad7fe1
dmonto3 <dmon501@gmail.com>
2014-09-08 11:54:54 -0500

    instance Eq

--- RegEx.hs
+++ RegEx.hs
@@ -10,4 +10,17 @@ data RegExp a = Empty
               | Star (RegExp a)
               | Plus (RegExp a) (RegExp a)
               | Dot  (RegExp a) (RegExp a)
-                deriving (Show, Eq)
+
+instance (Eq a) => Eq (RegExp a) where
+  Epsilon      == Epsilon      = True
+  Epsilon      == _            = False
+  Empty        == Empty        = True
+  Empty        == _            = False
+  (Symbol a)   == (Symbol b)   = a == b
+  (Symbol a)   == _            = False
+  (Star e)     == (Star f)     = e == f
+  (Star e)     == _            = False
+  (Plus e1 e2) == (Plus f1 f2) = (e1 == f1) && (e2 == f2)
+  (Plus e1 e2) == _            = False
+  (Dot e1 e2)  == (Dot f1 f2)  = (e1 == f1) && (e2 == f2)
+  (Dot e1 e2)  == _            = False
