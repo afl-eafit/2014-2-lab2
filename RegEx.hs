2730574fcff29fa2648ca8877b1c0569dcbcc033
dmonto3 <dmon501@gmail.com>
2014-09-08 19:43:15 -0500

    deriving Eq

--- RegEx.hs
+++ RegEx.hs
@@ -4,23 +4,10 @@
 module RegEx where
 
 -- A polymorphic data regular expression data type
-data RegExp a = Empty
-              | Epsilon
-              | Symbol a
-              | Star (RegExp a)
-              | Plus (RegExp a) (RegExp a)
-              | Dot  (RegExp a) (RegExp a)
-
-instance (Eq a) => Eq (RegExp a) where
-  Epsilon      == Epsilon      = True
-  Epsilon      == _            = False
-  Empty        == Empty        = True
-  Empty        == _            = False
-  (Symbol a)   == (Symbol b)   = a == b
-  (Symbol a)   == _            = False
-  (Star e)     == (Star f)     = e == f
-  (Star e)     == _            = False
-  (Plus e1 e2) == (Plus f1 f2) = (e1 == f1) && (e2 == f2)
-  (Plus e1 e2) == _            = False
-  (Dot e1 e2)  == (Dot f1 f2)  = (e1 == f1) && (e2 == f2)
-  (Dot e1 e2)  == _            = False
+data (Eq a) => RegExp a = Empty
+                        | Epsilon
+                        | Symbol a
+                        | Star (RegExp a)
+                        | Plus (RegExp a) (RegExp a)
+                        | Dot  (RegExp a) (RegExp a)
+                          deriving (Eq)
